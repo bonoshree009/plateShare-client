9dbdacd4e7d1c8555fd074eee63ef4aa18f003af
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2026-02-12 21:47:40 +0600

    updated

--- src/Component/LayOut/Banner.jsx
+++ src/Component/LayOut/Banner.jsx
@@ -21,27 +21,34 @@ const Banner = () => {
         transition={{ duration: 0.8, ease: "easeOut" }}
         className="relative py-30 px-auto text-center text-white"
       >
-        <h1 className="text-6xl font-bold mb-4">
-          Delicious Food, Anytime
-        </h1>
+        <motion.h1
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.3 }}
+          className="text-4xl md:text-6xl font-bold leading-tight"
+        >
+         Delicious Food, Anytime
+        </motion.h1>
 
-        <p className="text-lg max-w-2xl mx-auto mb-6">
-          A community-driven platform to share food, reduce waste, and help others.
-          Discover available foods near you.
-        </p>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.8 }}
+          className="mt-6 text-lg md:text-xl text-gray-200"
+        >
+          A community-driven platform to share food, reduce waste, and help others. Discover available foods near you.
+        </motion.p>
+        
 <motion.nav
   whileHover={{ scale: 1.05 }}
   whileTap={{ scale: 0.95 }}
 >
   <NavLink
     to="/available-foods"
-    className="bg-green-600 px-6 py-3 rounded-xl text-lg font-semibold 
-                transition  duration-300 buttonbanner buttonin flex items-center gap-2"
-  >
-    <div className="hoverEffect">
-      <div></div>
-    </div>
-    Available Foods
+    className="bg-green-600 px-6 py-3 rounded-xl text-lg font-semibold mt-3
+                transition  duration-300 buttonbanner buttonin flex items-cente"
+  > Expolore Food
+   
   </NavLink>
 </motion.nav>
 
